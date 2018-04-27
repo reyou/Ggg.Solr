118bfd31615e7e0abc2944d15b6964bc543a75a7
Teoman Shipahi <aytekoz@yahoo.com>
2018-04-26 20:16:03 -0400

    promise.all

--- apps/app-nodejs/Providers/create-random-users.js
+++ apps/app-nodejs/Providers/create-random-users.js
@@ -1,37 +1,42 @@
-var request = require('request');
+var request = require("request");
 
 class RandomUserGenerator {
-    constructor(core) {
-        this.core = core;
+  constructor(core) {
+    this.core = core;
+  }
+  createRandomUsers(count) {
+    let promises = [];
+    for (var i = 0; i <= count; i++) {
+      let promise = new Promise(function(resolve, reject) {
+        request("https://api.randomuser.me/", function(error, response, body) {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(body);
+          }
+        });
+      });
+      promises.push(promise);
     }
-    createRandomUsers(count) {
-        let promises = [];
-        for (var i = 0; i <= count; i++) {
-            let promise = new Promise(function (resolve, reject) {
-                request('https://api.randomuser.me/', function (error, response, body) {
-                    if (error) {
-                        reject(error);
-                    }
-                    else {
-                        resolve(response, body)
-                    }
-                });
-            });
-            promises.push(promise);
-        }
-        return promises;
-    }
+    return promises;
+  }
 }
-
 
 let randomUserGenerator = new RandomUserGenerator();
 let tasks = randomUserGenerator.createRandomUsers(10);
-let onTasksCompleted = function (completedTasks) {
-    for (let i = 0; i <= completedTasks.length; i++) {
-        debugger;
-    }
-}
-let onTasksCatch = function (error) {
-    console.log("onTasksCatch", error);
-}
-Promise.all(tasks).then(onTasksCompleted).catch(onTasksCatch);
+let onTasksCompleted = function(completedTasks) {
+  for (let i = 0; i <= completedTasks.length; i++) {
+    let completedTask = completedTasks[i];
+    console.log("completedTask:", completedTask);
+  }
+};
+let onTasksCatch = function(error) {
+  console.log("onTasksCatch", error);
+};
+/*Creates a Promise that is resolved with an array 
+of results when all of the provided Promises resolve, 
+or rejected when any Promise is rejected.*/
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/all
+Promise.all(tasks)
+  .then(onTasksCompleted)
+  .catch(onTasksCatch);
